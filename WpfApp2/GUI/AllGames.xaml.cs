9406566f55f2b86353261b18b23625325d676c47
Natalie Elisha <natalie7399@gmail.com>
2018-12-11 08:48:24 +0200

    started my games and all games

---
+++ WpfApp2/GUI/AllGames.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2.GUI
+{
+    /// <summary>
+    /// Interaction logic for AllGames.xaml
+    /// </summary>
+    public partial class AllGames : Window, INotifyPropertyChanged
+    {
+        #region Properties
+        //public DelegateCommand<object> GoToSettings { get; set; }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public AllGames()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public void NotifyPropertyChanged(string prop)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+    }
+}
---
+++ WpfApp2/GUI/MyGames.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MuSearch.DB;
+
+namespace MuSearch.GUI
+{
+    /// <summary>
+    /// Interaction logic for MyGames.xaml
+    /// </summary>
+    public partial class MyGames : Window
+    {
+        List<Score> lst;
+
+        public MyGames()
+        {
+            InitializeComponent();
+            Score first = new Score("12.1.17", 200);
+            Score second = new Score("29.4.12", 870);
+            lst.Add(first);
+            lst.Add(second);
+        }
+    }
+}
